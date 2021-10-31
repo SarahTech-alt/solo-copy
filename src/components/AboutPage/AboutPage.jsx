e7554caaac792124cf4372038cfd06881f4abd2f
Sarah Fuoss <sfuoss@gmail.com>
2021-10-31 12:03:38 -0500

    Updated About page.

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -10,8 +10,9 @@ function AboutPage() {
     <div className="container">
     
       <div>
-        <p>Never Lose Your Mushroom Spot Again!</p>
-
+        <h2>Never Lose Your Mushroom Spot Again!</h2>
+        <p>Keeping track of all the mushrooms you find in the wild can be a difficult task. Using a map with pins alone is inadequate to provide all the information you might want to revisit. ShroomLogger enables you to upload pictures, descriptions and locations to create a complete history of your favorite fungi locations.</p>
+        <p>Comments or suggestions can be sent to sfuoss@gmail.com.</p>
  <footer>ShroomLogger &copy; Sarah Fuoss</footer>
       </div>
     </div>
